2ac4942e425681cddf340a245e6812ace0b95761
Alex Radulescu <alex.radulescu@hopin.to>
2021-03-09 14:53:44 +0200

    Extract Header into a separate component

---
+++ components/Header.jsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import { Container } from 'react-bootstrap'
+
+const Header = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Daily Facilitator</title>
+      </Head>
+      <Container fluid className="header">
+        <h1 className="title">
+          Welcome to the Daily Roulette!
+        </h1>
+
+        <p className="subtitle">Please select your preferred update sharing presentation order:</p>
+      </Container>
+    </Container>
+  )
+}
+
+export default Header
--- pages/account.jsx
+++ pages/account.jsx
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Header from '../components/Header'
 import { Container, Row, Button } from 'react-bootstrap'
 import SignIn from '../components/account/SignIn'
 import SignUp from '../components/account/SignUp'
@@ -6,14 +6,7 @@ import SignUp from '../components/account/SignUp'
 export default function Account() {
   return (
     <Container fluid className="page">
-      <Head>
-        <title>Daily Facilitator</title>
-      </Head>
-      <Container fluid className="header">
-        <h1 className="title">
-          Welcome to the Daily Roulette!
-        </h1>
-      </Container>
+      <Header />
 
       <Container className="content">
           <Row className="justify-content-md-between">
