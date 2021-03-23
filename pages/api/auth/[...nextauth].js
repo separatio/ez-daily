e45f2903072aa1304f25ecf1fd9b7c874fc2769e
Alex Radulescu <alex.radulescu@hopin.to>
2021-03-23 10:49:41 +0200

    Add new eslint rule exclusions

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,32 +1,32 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
-import Adapters from 'next-auth/adapters'
-import { PrismaClient } from '@prisma/client'
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+import Adapters from 'next-auth/adapters';
+import {PrismaClient} from '@prisma/client';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export default NextAuth({
   providers: [
     Providers.Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
+      clientSecret: process.env.GITHUB_SECRET,
     }),
     Providers.GitLab({
       clientId: process.env.GITLAB_CLIENT_ID,
-      clientSecret: process.env.GITLAB_CLIENT_SECRET
+      clientSecret: process.env.GITLAB_CLIENT_SECRET,
     }),
     Providers.Atlassian({
       clientId: process.env.ATLASSIAN_CLIENT_ID,
       clientSecret: process.env.ATLASSIAN_CLIENT_SECRET,
-      scope: 'write:jira-work read:jira-work read:jira-user offline_access read:me'
-    })
+      scope: 'write:jira-work read:jira-work read:jira-user offline_access read:me',
+    }),
   ],
   pages: {
-    signIn: '/signin'
+    signIn: '/signin',
   },
-  adapter: Adapters.Prisma.Adapter({ prisma }),
-})
+  adapter: Adapters.Prisma.Adapter({prisma}),
+});
