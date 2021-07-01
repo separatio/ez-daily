536f55b51e88883f1416bdebdba933db49196ba1
Alex Radulescu <a.radulescu@toptal.com>
2021-07-01 19:42:35 +0300

    Extract cards data and ChoiceCardsList component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,31 +1,51 @@
 import Layout from '../components/Layout';
 import ChoiceCard from '../components/ChoiceCard';
 
+const choiceCardsData = [
+  {
+    id: 1,
+    title: 'Roulette',
+    text: 'Your team members, in Random order!',
+  },
+  {
+    id: 2,
+    title: 'As-is',
+    text: 'Use a custom order for the team members!',
+  },
+  {
+    id: 3,
+    title: 'Alphabetical',
+    text: 'This is self-explanatory, right?',
+  },
+  {
+    id: 4,
+    title: 'Reverse Alphabetical',
+    text: 'Play the Roulette in Reverse!',
+  },
+];
+
+const ChoiceCardsList = () => {
+  return (
+    <div className="row row-cols-1 row-cols-md-2">
+      {
+        choiceCardsData.map((props) => {
+          return (
+            <ChoiceCard
+              {...props}
+              key={props.id}
+            />
+          );
+        })
+      }
+    </div>
+  );
+};
+
 export default function Page() {
   return (
     <Layout>
       <div className="container content">
-        <div className="row row-cols-1 row-cols-md-2">
-          <ChoiceCard
-            title='Roulette'
-            text='Your team members, in Random order!'
-          />
-
-          <ChoiceCard
-            title='As-is'
-            text='Use a custom order for the team members!'
-          />
-
-          <ChoiceCard
-            title='Alphabetical'
-            text='This is self-explanatory, right?'
-          />
-
-          <ChoiceCard
-            title='Reverse Alphabetical'
-            text='Play the Roulette in Reverse!'
-          />
-        </div>
+        <ChoiceCardsList />
       </div>
     </Layout>
   );
