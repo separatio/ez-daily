e41485068baeb57e30e41db200664c6718025d27
Alex Radulescu <alex.radulescu@toptal.com>
2021-12-12 09:33:23 +0200

    Simplify landing page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,12 @@
-import { useSession } from 'next-auth/react'
 import Head from 'next/head'
 
 export default function Header({ title = 'Daily Facilitator' }) {
-  const { data: session, status } = useSession()
-  const loading = status === 'loading'
-
   return (
-    <header>
-      <Head>
-        <title>{title}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-
-      <div className="container-fluid header">
-        <h1 className="title">Welcome to the Daily Roulette!</h1>
-      </div>
-    </header>
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="description" content="The Daily app for your Daily needs" />
+    </Head>
   )
 }
