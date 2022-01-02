ddf6529a187c287643f26440f4b34b80ec8015f4
Alex Radulescu <alex.radulescu@toptal.com>
2022-01-02 14:48:51 +0200

    Implement daily roulette main page

--- pages/daily.tsx
+++ pages/daily.tsx
@@ -1,10 +1,71 @@
+import { Button, Grid, Typography } from '@mui/material'
 import type { NextPage } from 'next'
+import { useState } from 'react'
+
+let memberList = [
+  'Aleksandr',
+  'Alex',
+  'Andrei',
+  'Denys',
+  'Huy',
+  'Ilya',
+  'Kavitha',
+  'Rafal',
+  'Sergiu',
+  'Timofei',
+]
+
+function selectTeamMember() {
+  let member = ''
+
+  const membersListLength = memberList.length
+  const randomTeamMember = Math.floor(Math.random() * membersListLength)
+
+  if (membersListLength === 0) return 'Done!'
+  else {
+    member = memberList[randomTeamMember]
+    memberList.splice(randomTeamMember, 1)
+
+    return member
+  }
+}
 
 const Daily: NextPage = () => {
+  const [teamMember, setTeamMember] = useState("Let's start!")
+
   return (
-    <>
-      <h1>This is the Daily page</h1>
-    </>
+    <Grid
+      container
+      direction="column"
+      justifyContent="flex-start"
+      alignItems="center"
+      rowSpacing={10}
+      sx={{
+        paddingTop: '10rem',
+      }}
+    >
+      {' '}
+      <Button
+        variant="contained"
+        sx={{
+          fontSize: '5rem',
+        }}
+        onClick={() => {
+          setTeamMember((teamMember) => {
+            return selectTeamMember()
+          })
+        }}
+      >
+        Daily Roulette!
+      </Button>{' '}
+      <Typography
+        sx={{
+          fontSize: '5rem',
+        }}
+      >
+        <h1>{teamMember}</h1>
+      </Typography>
+    </Grid>
   )
 }
 
