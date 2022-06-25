185796868a0b36553fa9e156ab4c92d9fec1b4a7
Alex Radulescu <alex.radulescu@toptal.com>
2022-06-25 23:39:55 +0200

    add new team functionality

--- pages/api/teams/create.ts
+++ pages/api/teams/create.ts
@@ -2,25 +2,20 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../../src/lib/prisma'
 import { getSession } from 'next-auth/react'
 
-// POST /api/teams
+// POST /api/teams/create
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const session = await getSession({ req })
 
-  if (session) {
-    const user =
-      session.user &&
-      (await prisma.user.findUnique({
-        where: { email: session.user.email || undefined },
-      }))
+  const { title } = req.body
 
-    // const result = await prisma.team.create({
-    //   data: {
-    //     title: 'test',
-    //     author: user || undefined
-    //   }
-    // })
-  }
+  const result = await prisma.team.create({
+    data: {
+      title: title,
+      author: { connect: { email: session?.user?.email || undefined } },
+    },
+  })
+  res.json(result)
 }
--- src/components/teams/AddTeamForm.tsx
+++ src/components/teams/AddTeamForm.tsx
@@ -1,16 +1,37 @@
 import { Input, InputLabel } from '@mui/material'
 import { FormControl } from '@mui/material'
+import Router from "next/router";
+import { useState } from 'react'
 
 const AddTeamForm = () => {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    try {
+      const body = { title };
+      await fetch(`/api/teams/create`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      await Router.push("/teams");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <FormControl
-      sx={{
-        minWidth: 226,
-      }}
-    >
-      <InputLabel htmlFor="my-input">+ Add Team</InputLabel>
-      <Input id="add-team" />
-    </FormControl>
+    <form onSubmit={handleSubmit}>
+      <FormControl
+        sx={{
+          minWidth: 226,
+        }}
+      >
+        <InputLabel htmlFor="my-input">+ Add Team</InputLabel>
+        <Input id="add-team" onChange={(e) => setTitle(e.target.value)} sx={{ color: 'black'}}/>
+      </FormControl>
+    </form>
   )
 }
 
